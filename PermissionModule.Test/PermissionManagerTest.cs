f304432f488cbe3ffc6d728a2a6bd03fd0e3e46d
Kaan Yilgin <kaan@mcb.local>
2020-01-23 22:14:29 +0100

    Asserting ApplyPropertyTypeRelatedPermissions function

--- PermissionModule.Test/PermissionManagerTest.cs
+++ PermissionModule.Test/PermissionManagerTest.cs
@@ -62,6 +62,58 @@ namespace PermissionModule.UnitTest
             var actionPermission = permissionModels.FirstOrDefault(x => x.Action == actionEnum);
             Assert.That(actionPermission, Is.Not.Null, $"There is no permission model for action type: {actionEnum}" );
         }
+        
+        [TestCase(ActionEnum.MenuViewProperty)]
+        [TestCase(ActionEnum.ShareProperty)]
+        [TestCase(ActionEnum.SendingDocuments)]
+        [TestCase(ActionEnum.MenuScheduleViewing)]
+        [TestCase(ActionEnum.ContextMenuMoreInformation)]
+        [TestCase(ActionEnum.AddPhoto)]
+        [TestCase(ActionEnum.MenuAskQuestion)]
+        [TestCase(ActionEnum.MenuReplyQuestion)]
+        [TestCase(ActionEnum.MenuAddFavourites)]
+        [TestCase(ActionEnum.MessageToAgent)]
+        [TestCase(ActionEnum.ViewPhoneCallOfAgent)]
+        [TestCase(ActionEnum.DownloadBrochure)]
+        [TestCase(ActionEnum.ViewLocation)]
+        [TestCase(ActionEnum.RequestEnergyLabel)]
+        [TestCase(ActionEnum.Request360Video)]
+        public void CreatePermissionModel_WhenActionEnumHasNoRestriction_ShouldIsVisibleAndIsEnableAlwaysEqualToTrue(ActionEnum actionEnum)
+        {
+            //Arrange
+            this.callContext.IsPrivate = true;
+            
+            // Act
+            var permissionModels = this.sut.GetPermissions(this.callContext);
+
+            // Assert
+            var actionPermission = permissionModels.FirstOrDefault(x => x.Action == actionEnum);
+            Assert.That(actionPermission.IsEnabled, Is.True, $"Is enabled is not true for action type: {actionEnum}" );
+            Assert.That(actionPermission.IsVisible, Is.True, $"Is enabled is not true for action type: {actionEnum}" );
+        }
+        
+        [TestCase(ActionEnum.DeleteProperty, true)]
+        [TestCase(ActionEnum.DeleteProperty, false)]
+        [TestCase(ActionEnum.MakeOffer, true)]
+        [TestCase(ActionEnum.MakeOffer, false)]
+        [TestCase(ActionEnum.ChangeBiddingPrice, true)]
+        [TestCase(ActionEnum.ChangeBiddingPrice, false)]
+        [TestCase(ActionEnum.CheckBiddingStatus, true)]
+        [TestCase(ActionEnum.CheckBiddingStatus, false)]
+        public void CreatePermissionModel_WhenActionTypeDependsOnIsPrivate_ShouldIsEnabledAndIsVisibleAreEqualToInverseOfIsPrivate(ActionEnum actionType, bool isPublished)
+        {
+            // Arrange
+            this.callContext.IsPrivate = isPublished;
+            
+            // Act
+            var permissionModels = this.sut.GetPermissions(this.callContext);
+
+            // Assert
+            var actionPermission = permissionModels.First(x => x.Action == actionType);
+            Assert.That(actionPermission.IsEnabled, Is.EqualTo(!isPublished));
+            Assert.That(actionPermission.IsVisible, Is.EqualTo(!isPublished));
+        }
+
 
         private void InitializeVariables()
         {
